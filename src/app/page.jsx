001cf71490345a0dfe254b568597dc0c54a6911d
Shriyash shukla <139564061+shriyashshukla@users.noreply.github.com>
2024-06-11 11:45:42 +0530

    chore: Add paths for app/* in jsconfig.json and update layout.js and globals.css

---
+++ src/app/page.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <Link href="/">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+          ipsum odio dolore, nemo placeat adipisci amet, facere distinctio dicta
+          quis reprehenderit rem nobis quidem libero eaque dolor vitae ullam.
+          Dicta.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui,
+          mollitia! Quisquam eos error explicabo odit aliquid, itaque,
+          temporibus cum voluptatum repellendus, labore pariatur deleniti eaque
+          illo accusamus optio perferendis similique!Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Qui, mollitia! Quisquam eos error
+          explicabo odit aliquid, itaque, temporibus cum voluptatum repellendus,
+          labore pariatur deleniti eaque illo accusamus optio perferendis
+          similique!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Qui, mollitia! Quisquam eos error explicabo odit aliquid, itaque,
+          temporibus cum voluptatum repellendus, labore pariatur deleniti eaque
+          illo accusamus optio perferendis similique!Lorem ipsum dolor sit amet,
+          consectetur adipisicing elit. Qui, mollitia! Quisquam eos error
+          explicabo odit aliquid, itaque, temporibus cum voluptatum repellendus,
+          labore pariatur deleniti eaque illo accusamus optio perferendis
+          similique!Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Qui, mollitia! Quisquam eos error explicabo odit aliquid, itaque,
+          temporibus cum voluptatum repellendus, labore pariatur deleniti eaque
+          illo accusamus optio perferendis similique!
+        </p>
+      </Link>
+    </div>
+  );
+};
+
+export default Page;
