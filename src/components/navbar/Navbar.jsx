63672dda9800f034d9d0d78167fe018031dfc137
Shriyash shukla <139564061+shriyashshukla@users.noreply.github.com>
2024-07-02 02:07:06 +0530

    chore: Update navbar links in Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,8 +10,15 @@ const Navbar = () => {
       <Image src="/twitter.png" alt="twitter" width={24 } height={24}/>
       </div>
       <div className={styles.logo}></div>
-      <div className={styles.links}></div>
-      <div className={styles.social}></div>
+      <div className={styles.links}>
+       
+        <Link>about</Link>
+        <Link>services</Link>
+        <Link>contact</Link>
+        <Link>login</Link>
+
+      </div>
+      
 
       
 </div>
